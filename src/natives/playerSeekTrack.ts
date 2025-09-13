ba3b791169a8a4281981e04440d56fe87c19b00a
Zack-911 <muhammadbinasim471@gmail.com>
2025-09-13 05:02:13 +0200

    Added $playerSeekTrack

---
+++ src/natives/playerSeekTrack.ts
@@ -0,0 +1,32 @@
+import { NativeFunction, ArgType } from "@tryforge/forgescript";
+import { ForgeLinked } from "../index.js";
+
+export default new NativeFunction({
+  name: '$playerSeekTrack',
+  description: 'Seek a track in a player',
+  brackets: true,
+  unwrap: true,
+  args: [
+    {
+      name: 'guildId',
+      description: 'The guild id to seek the track for',
+      rest: false,
+      type: ArgType.Guild,
+      required: true,
+    },
+    {
+      name: 'position',
+      description: 'The position to seek to',
+      type: ArgType.Number,
+      rest: false,
+      required: true,
+    }
+  ],
+  async execute(ctx, [guildId, position]) {
+    const linked = ctx.client.getExtension(ForgeLinked, true).kazagumo
+    const player = linked.players.get(guildId.id)
+    if (!player) return this.customError('No player found for this guild')
+    player.seek(position)
+    return this.success()
+  }
+})

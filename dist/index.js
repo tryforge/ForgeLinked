6b955cb6a4661b92fc570f8795fc1837f67a3a46
Zack <muhammadbinasim471@gmail.com>
2026-04-24 08:04:14 +0500

    chore: bump package version from 2.1.0 to 2.2.1

--- dist/index.js
+++ dist/index.js
@@ -16,7 +16,7 @@ class ForgeLinked extends forgescript_1.ForgeExtension {
     options;
     name = 'ForgeLink';
     description = 'ForgeScript integration with lavalink-client';
-    version = '2.1.0';
+    version = '2.2.1';
     client;
     lavalink;
     commands;
